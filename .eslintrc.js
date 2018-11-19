31db823fe71c25d87ceb3d312c821cdeb1608bf4
Spencer Carli <spencercarli@gmail.com>
2018-11-19 11:08:50 -0600

    House keeping

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ["airbnb", "eslint-config-prettier"],
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    jest: true
+  },
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "react/prop-types": 0,
+    "react/destructuring-assignment": 0,
+    "global-require": 0,
+    "import/no-unresolved": 0,
+    "react/prefer-stateless-function": 0,
+    "prefer-destructuring": 0,
+    "import/prefer-default-export": 0,
+    "no-underscore-dangle": 0,
+    "react/no-multi-comp": 0,
+    "func-names": 0,
+    "no-alert": 0
+  }
+};
