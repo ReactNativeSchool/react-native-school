9d4ac9f2584e6e82dd91e090d1c57a070b0cae29
Spencer Carli <spencercarli@gmail.com>
2018-11-28 11:32:44 -0600

    Deploy to now.

--- courses/image-upload/server/index.js
+++ courses/image-upload/server/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 const Storage = multer.diskStorage({
   destination(req, file, callback) {
-    callback(null, "./images");
+    callback(null, process.env.NODE_ENV === "production" ? "/tmp" : "./images");
   },
   filename(req, file, callback) {
     callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
@@ -15,6 +15,12 @@ const Storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: Storage });
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("You can post to /api/upload-single or /api/upload-multiple.");
+});
 
 app.post("/api/upload-single", upload.single("photo"), (req, res) => {
   console.log("file", req.file);
