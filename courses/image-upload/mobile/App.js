cb75a4a5d0ad12fbf8874832987cd16ad7863434
Spencer Carli <spencercarli@gmail.com>
2018-11-29 10:34:51 -0600

    Upload photo with progress

--- courses/image-upload/mobile/App.js
+++ courses/image-upload/mobile/App.js
@@ -19,12 +19,35 @@ const createFormData = (photo, body) => {
   return data;
 };
 
+const uploadFileWithProgress = (url, opts = {}, onProgress) =>
+  new Promise((res, rej) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(opts.method || "get", url);
+
+    Object.keys(opts.headers || {}).forEach(value => {
+      xhr.setRequestHeader(value, opts.headers[value]);
+    });
+
+    if (xhr.upload && onProgress) {
+      xhr.upload.onprogress = onProgress;
+    }
+
+    xhr.onload = e => {
+      res(e.target.response);
+    };
+    xhr.onerror = rej;
+
+    xhr.send(opts.body);
+  });
+
 export default class App extends React.Component {
   state = {
-    photo: null
+    photo: null,
+    progress: 0
   };
 
   handleUploadPhoto = () => {
+    /*
     fetch("http://localhost:3000/api/upload", {
       method: "POST",
       body: createFormData(this.state.photo, { userId: "123" })
@@ -34,6 +57,28 @@ export default class App extends React.Component {
         console.log("upload succes", response);
         alert("Upload success!");
         this.setState({ photo: null });
+      })
+      .catch(error => {
+        console.log("upload error", error);
+        alert("Upload failed!");
+      });
+    */
+    uploadFileWithProgress(
+      "http://localhost:3000/api/upload",
+      {
+        method: "POST",
+        body: createFormData(this.state.photo, { userId: "123" })
+      },
+      event => {
+        const progress = Math.floor((event.loaded / event.total) * 100);
+        console.log("progress", progress);
+        this.setState({ progress });
+      }
+    )
+      .then(response => {
+        console.log("upload succes", response);
+        alert("Upload success!");
+        this.setState({ photo: null, progress: 0 });
       })
       .catch(error => {
         console.log("upload error", error);
@@ -53,7 +98,7 @@ export default class App extends React.Component {
   };
 
   render() {
-    const { photo } = this.state;
+    const { photo, progress } = this.state;
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         {photo && (
@@ -62,6 +107,11 @@ export default class App extends React.Component {
               source={{ uri: photo.uri }}
               style={{ width: 300, height: 300 }}
             />
+            <Text>
+              Progress:
+              {progress}
+%
+            </Text>
             <Button title="Upload Photo" onPress={this.handleUploadPhoto} />
           </React.Fragment>
         )}
