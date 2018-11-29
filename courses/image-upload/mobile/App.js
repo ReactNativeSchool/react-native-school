2a7ce81351d439cd5a65d1c3266e955fb05d2a38
Spencer Carli <spencercarli@gmail.com>
2018-11-29 10:21:36 -0600

    Upload with fetch

--- courses/image-upload/mobile/App.js
+++ courses/image-upload/mobile/App.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { View, Text, Image, Button } from "react-native";
+import { View, Text, Image, Button, Platform } from "react-native";
 import ImagePicker from "react-native-image-picker";
+
+const createFormData = (photo, body) => {
+  const data = new FormData();
+
+  data.append("photo", {
+    name: photo.fileName,
+    type: photo.type,
+    uri:
+      Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
+  });
+
+  Object.keys(body).forEach(key => {
+    data.append(key, body[key]);
+  });
+
+  return data;
+};
 
 export default class App extends React.Component {
   state = {
     photo: null
+  };
+
+  handleUploadPhoto = () => {
+    fetch("http://localhost:3000/api/upload", {
+      method: "POST",
+      body: createFormData(this.state.photo, { userId: "123" })
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log("upload succes", response);
+        alert("Upload success!");
+        this.setState({ photo: null });
+      })
+      .catch(error => {
+        console.log("upload error", error);
+        alert("Upload failed!");
+      });
   };
 
   handleChoosePhoto = () => {
@@ -23,10 +57,13 @@ export default class App extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         {photo && (
-          <Image
-            source={{ uri: photo.uri }}
-            style={{ width: 300, height: 300 }}
-          />
+          <React.Fragment>
+            <Image
+              source={{ uri: photo.uri }}
+              style={{ width: 300, height: 300 }}
+            />
+            <Button title="Upload Photo" onPress={this.handleUploadPhoto} />
+          </React.Fragment>
         )}
         <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
       </View>
