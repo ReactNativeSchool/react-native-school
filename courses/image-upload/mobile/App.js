c635491223c6d5283bdd199027500518a6bd66e0
Spencer Carli <spencercarli@gmail.com>
2018-11-29 09:21:11 -0600

    Setup react-native-image-picker

--- courses/image-upload/mobile/App.js
+++ courses/image-upload/mobile/App.js
@@ -1,49 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import { View, Text, Image, Button } from "react-native";
+import ImagePicker from "react-native-image-picker";
 
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+export default class App extends React.Component {
+  state = {
+    photo: null
+  };
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+  handleChoosePhoto = () => {
+    const options = {
+      noData: true
+    };
+    ImagePicker.launchImageLibrary(options, response => {
+      if (response.uri) {
+        this.setState({ photo: response });
+      }
+    });
+  };
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
+    const { photo } = this.state;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        {photo && (
+          <Image
+            source={{ uri: photo.uri }}
+            style={{ width: 300, height: 300 }}
+          />
+        )}
+        <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
