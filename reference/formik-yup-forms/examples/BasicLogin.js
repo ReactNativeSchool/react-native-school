84d97f75a3c3a2b0b2f9ecb034835cf1cb28421f
Spencer Carli <spencercarli@gmail.com>
2018-12-07 09:16:31 -0600

    Add a different field type

--- reference/formik-yup-forms/examples/BasicLogin.js
+++ reference/formik-yup-forms/examples/BasicLogin.js
@@ -1,24 +1,50 @@
 import React from "react";
-import { View, Text, Button, TextInput, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  ActivityIndicator,
+  Switch
+} from "react-native";
 import { Formik } from "formik";
 import * as yup from "yup";
 
-const StyledInput = ({ formikKey, formikProps, ...rest }) => {
+const FieldWrapper = ({ formikProps, formikKey, label, children }) => {
+  const labelStyles = {
+    marginBottom: 3
+  };
+
+  let errorMessage = "";
+
+  if (formikProps.errors[formikKey] && formikProps.touched[formikKey]) {
+    labelStyles.color = "red";
+    errorMessage = formikProps.errors[formikKey];
+  }
+
+  return (
+    <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+      <Text style={labelStyles}>{label}</Text>
+      {children}
+      <Text style={{ color: "red" }}>{errorMessage}</Text>
+    </View>
+  );
+};
+
+const StyledInput = ({ formikKey, formikProps, label, ...rest }) => {
   const inputStyles = {
     borderWidth: 1,
     borderColor: "black",
     padding: 10,
     marginBottom: 3
   };
-  let errorMessage = "";
 
   if (formikProps.errors[formikKey] && formikProps.touched[formikKey]) {
     inputStyles.borderColor = "red";
-    errorMessage = formikProps.errors[formikKey];
   }
 
   return (
-    <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+    <FieldWrapper formikKey={formikKey} label={label} formikProps={formikProps}>
       <TextInput
         style={inputStyles}
         autoCorrect={false}
@@ -26,10 +52,21 @@ const StyledInput = ({ formikKey, formikProps, ...rest }) => {
         onBlur={formikProps.handleBlur(formikKey)}
         {...rest}
       />
-      <Text style={{ color: "red" }}>{errorMessage}</Text>
-    </View>
+    </FieldWrapper>
   );
 };
+
+const StyledSwitch = ({ formikKey, formikProps, label, ...rest }) => (
+  <FieldWrapper formikKey={formikKey} label={label} formikProps={formikProps}>
+    <Switch
+      value={formikProps.values[formikKey]}
+      onValueChange={value => {
+        formikProps.setFieldValue(formikKey, value);
+      }}
+      {...rest}
+    />
+  </FieldWrapper>
+);
 
 const validationSchema = yup.object().shape({
   email: yup
@@ -42,13 +79,22 @@ const validationSchema = yup.object().shape({
     .label("Password")
     .required()
     .min(2, "Seems a bit short...")
-    .max(10, "We prefer insecure systems, shorten your password.")
+    .max(10, "We prefer insecure systems, shorten your password."),
+  agreeToTerms: yup
+    .boolean()
+    .label("terms")
+    .test(
+      "is-true",
+      // eslint-disable-next-line
+      "Must agree to ${path} to continue",
+      value => value === true
+    )
 });
 
 const BasicLogin = () => (
   <View style={{ flex: 1, justifyContent: "center" }}>
     <Formik
-      initialValues={{ email: "", password: "" }}
+      initialValues={{ email: "", password: "", agreeToTerms: false }}
       validationSchema={validationSchema}
       onSubmit={(values, actions) => {
         setTimeout(() => {
@@ -60,15 +106,22 @@ const BasicLogin = () => (
       {formikProps => (
         <React.Fragment>
           <StyledInput
+            label="Email"
             placeholder="Email"
             formikKey="email"
             formikProps={formikProps}
           />
           <StyledInput
+            label="Password"
             placeholder="Password"
             formikKey="password"
             formikProps={formikProps}
             secureTextEntry
+          />
+          <StyledSwitch
+            label="Agree to Terms"
+            formikKey="agreeToTerms"
+            formikProps={formikProps}
           />
           {formikProps.isSubmitting ? (
             <ActivityIndicator />
