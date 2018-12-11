6273651ee6f6608c9b8c8160daaf0bcca42c2408
Spencer Carli <spencercarli@gmail.com>
2018-12-11 09:45:49 -0600

    Basic formik form

--- courses/FormikYupForms/App.js
+++ courses/FormikYupForms/App.js
@@ -1,49 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import {
+  SafeAreaView,
+  TextInput,
+  Button,
+  ActivityIndicator
+} from "react-native";
+import { Formik } from "formik";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default () => (
+  <SafeAreaView style={{ marginTop: 90 }}>
+    <Formik
+      initialValues={{ name: "" }}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values));
+        setTimeout(() => {
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+    >
+      {formikProps => (
+        <React.Fragment>
+          <TextInput
+            style={{
+              borderWidth: 1,
+              borderColor: "black",
+              padding: 10,
+              marginBottom: 3
+            }}
+            onChangeText={formikProps.handleChange("name")}
+          />
+          {formikProps.isSubmitting ? (
+            <ActivityIndicator />
+          ) : (
+            <Button title="Submit" onPress={formikProps.handleSubmit} />
+          )}
+        </React.Fragment>
+      )}
+    </Formik>
+  </SafeAreaView>
+);
