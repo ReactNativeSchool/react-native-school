0fcb7248827814be43ef9a70f58abe64f3dd0423
Spencer Carli <spencercarli@gmail.com>
2019-01-10 11:58:05 -0600

    Build an actual form

--- courses/component-library/App/index.js
+++ courses/component-library/App/index.js
@@ -1,1 +1,19 @@
-export default from "../storybook";
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { Form, TextInput, Switch } from "./components/Form";
+import { Button } from "./components/Button";
+
+export default () => (
+  <SafeAreaView style={{ flex: 1 }}>
+    <Form header="Hello." subheader="Please create a new account">
+      <TextInput label="Email" keyboardType="email-address" />
+      <TextInput label="Password" secureTextEntry />
+      <TextInput label="Confirm Password" secureTextEntry />
+      <Switch label="Agree to Terms" />
+      <Button>Sign Up</Button>
+      <Button outline>Sign In</Button>
+    </Form>
+  </SafeAreaView>
+);
+
+// export default from "../storybook";
