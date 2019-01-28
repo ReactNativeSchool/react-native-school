0f732daa3011f553fd1d8d289f48e917bcb8dadb
Spencer Carli <spencercarli@gmail.com>
2019-01-28 11:39:30 -0600

    Basic app

--- reference/debugging/App.js
+++ reference/debugging/App.js
@@ -1,50 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+class App extends React.Component {
+  state = { number: null };
+
+  componentDidMount() {
+    this.getNumber();
+  }
+
+  getNumber = () => {
+    fetch("https://pond-relative.glitch.me/number")
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          number: res.number
+        });
+      });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.text}>{this.state.number}</Text>
+        <Button title="Get Number" onPress={this.getNumber} />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
